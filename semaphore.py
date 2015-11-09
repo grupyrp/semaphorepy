4a48541566a558c6d1d714aa0e57b4520bcfa8e1
Sergio Oliveira <seocam@seocam.com>
2015-11-09 15:22:37 -0200

    Created main_loop func

--- semaphore.py
+++ semaphore.py
@@ -13,7 +13,6 @@ program = sys.argv[1]
 program = os.path.abspath(program)
 
 
-
 def serial_ports():
     """ Lists serial port names
 
@@ -43,44 +42,48 @@ def serial_ports():
     return result
 
 
-ports = serial_ports()
-for port in ports:
-    print 'Testing port', port
-    arduino = serial.Serial(port, timeout=1)
-    time.sleep(2)
-    arduino.write('a')
-    line = arduino.read(10)
-    if line.strip() == 'semaphore':
-        print 'Found arduino semaphore at', port
-        break
-else:
-    print 'Arduino semaphone not found'
-    sys.exit(1)
+def main_loop():
+
+    ports = serial_ports()
+    for port in ports:
+        print 'Testing port', port
+        arduino = serial.Serial(port, timeout=1)
+        time.sleep(2)
+        arduino.write('a')
+        line = arduino.read(10)
+        if line.strip() == 'semaphore':
+            print 'Found arduino semaphore at', port
+            break
+    else:
+        print 'Arduino semaphone not found'
+        sys.exit(1)
+        md5 = None
+        last_md5 = None
+
+    while True:
+        time.sleep(0.5)
+
+        with open(program) as f:
+            md5 = hashlib.md5(f.read()).hexdigest()
+
+        if md5 == last_md5:
+            continue
+
+        arduino.write('y')
+        last_md5 = md5
+        try:
+            process = Popen([program])
+            process.wait()
+            if process.returncode == 0:
+                arduino.write('g')
+            else:
+                arduino.write('r')
+
+        except KeyboardInterrupt:
+            sys.exit()
+
+    arduino.close()
 
 
-md5 = None
-last_md5 = None
-
-while True:
-    time.sleep(0.5)
-
-    with open(program) as f:
-        md5 = hashlib.md5(f.read()).hexdigest()
-    
-    if md5 == last_md5:
-        continue
-        
-    arduino.write('y')
-    last_md5 = md5
-    try:
-        process = Popen([program])
-        process.wait()
-        if process.returncode == 0:
-            arduino.write('g')
-        else:
-            arduino.write('r')
-
-    except KeyboardInterrupt:
-        sys.exit()
-
-arduino.close()
+if __name__ == '__main__':
+    main_loop()
